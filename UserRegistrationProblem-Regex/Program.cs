e1391b59ed6a10a0a675e8d0a9cc838090e0223d
somu3341 <somashekarvsv1996@gmail.com>
2023-03-28 15:35:09 +0530

    [somu]Add:Validating User First Name

---
+++ UserRegistrationProblem-Regex/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UserRegistrationProblem_Regex;
+
+namespace Regex_UserRegistrationProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\nchoose option to Perform \n1.First Name \n2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                RegexProblems regular = new RegexProblems();
+                switch (option)
+                {
+                    case 1:
+                        regular.FirstName("Soma");
+                        break;                   
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ UserRegistrationProblem-Regex/RegexProblems.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblem_Regex
+{
+    public class RegexProblems
+    {
+        public const string First_Name = "^[A-Z]{1}[a-z]{3}$";
+        public void FirstName(string firstname)
+        {
+            if (Regex.IsMatch(firstname, First_Name))
+                Console.WriteLine("First Name Valid");
+            else
+                Console.WriteLine("Not Valid");
+        }
+    }
+}
