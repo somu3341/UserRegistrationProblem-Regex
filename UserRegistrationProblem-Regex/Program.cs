4a7a66002f8df261b1673c39e95d2e4492ac2ed0
somu3341 <somashekarvsv1996@gmail.com>
2023-03-28 16:26:15 +0530

    [somu]Add:Validating User Mobile Number

--- UserRegistrationProblem-Regex/Program.cs
+++ UserRegistrationProblem-Regex/Program.cs
@@ -10,7 +10,7 @@ namespace Regex_UserRegistrationProblem
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("\nchoose option to Perform \n1.First Name \n2.Last Name \n3.Email \n4.Exit");
+                Console.WriteLine("\nchoose option to Perform \n1.First Name \n2.Last Name \n3.Email \n4.Mobile Number \n5.Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 RegexProblems regular = new RegexProblems();
                 switch (option)
@@ -24,7 +24,10 @@ namespace Regex_UserRegistrationProblem
                     case 3:
                         regular.Email("abc.xyz@bl.co.in");
                         break;
-                    case 4:
+                        case 4:
+                        regular.Mobile("919919819801");
+                        break;
+                    case 5:
                         flag = false;
                         break;
                 }
--- UserRegistrationProblem-Regex/RegexProblems.cs
+++ UserRegistrationProblem-Regex/RegexProblems.cs
@@ -12,6 +12,7 @@ namespace UserRegistrationProblem_Regex
         public const string First_Name = "^[A-Z]{1}[a-z]{3}$";
         public const string Last_Name = "^[A-Z]{1}[a-z]{5}$";
         public const string EMAIL = "^[0-9a-zA-Z]+[.+_]{0,1}[0-9a-zA-Z]+[@][a-zA-Z]+[.][a-zA-Z]{2,3]([.][a-zA-Z]{2,3}){0,1}$";
+        public const string MOBILE = "^[1-9]{2}[6-9]{1}[0-9]{9}$";
         public void FirstName(string firstname)
         {
             if (Regex.IsMatch(firstname, First_Name))
@@ -30,6 +31,13 @@ namespace UserRegistrationProblem_Regex
         {
             if (Regex.IsMatch(email, EMAIL))
                 Console.WriteLine("Email is Valid");
+            else
+                Console.WriteLine("Not Valid");
+        }
+        public void Mobile(string mobile)
+        {
+            if (Regex.IsMatch(mobile, MOBILE))
+                Console.WriteLine("Mobile Number is Valid");
             else
                 Console.WriteLine("Not Valid");
         }
